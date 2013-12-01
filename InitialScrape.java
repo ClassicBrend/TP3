d718df8215af6bc96b5341c1926decf42a61b181
DrouinD <daviddrouin@hotmail.co.uk>
2013-12-01 21:39:40 +0000

    Small changes made

--- InitialScrape.java
+++ InitialScrape.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
